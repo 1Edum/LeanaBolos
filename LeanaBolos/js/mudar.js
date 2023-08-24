130134c1b50fcb582299e57b165816e8af7af874
1Edum <132601907+1Edum@users.noreply.github.com>
2023-08-24 10:50:55 -0300

    Add files via upload

---
+++ LeanaBolos/js/mudar.js
@@ -0,0 +1,18 @@
+function mouseOver() {
+    document.getElementById("wha").src = ('/img/whatsApp-mudar.png');
+}
+function mouseOut() {
+    document.getElementById("wha").src = ('/img/whatsApp.png');
+}
+function mouseover() {
+    document.getElementById("ins").src = ('/img/instagram-mudar.png');
+}
+function mouseout() {
+    document.getElementById("ins").src = ('/img/instagram.png');
+}
+function mouse0ver() {
+    document.getElementById("face").src = ('/img/facebook-mudar.png');
+}
+function mouse0ut() {
+    document.getElementById("face").src = ('/img/facebook.png');
+}
